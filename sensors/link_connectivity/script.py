8ef521e9359a3b16995c0ebc9363b2fbe81cd479
Kingman Network Manager <kngnm@bsc.coop>
2019-09-05 10:10:05 -0700

    Setup for micro-outage experiments

--- sensors/link_connectivity/script.py
+++ sensors/link_connectivity/script.py
@@ -16,14 +16,14 @@ influxdb_data = "link_connectivity,link={0} value=".format(
 )+"{0}"
 
 while True:
-    ping_output = os.popen("ping -f -q -c 100 -W 1 -s 10000 {0}".format(
+    ping_output = os.popen("ping -f -q -c 100 -W 1 {0}".format(
             os.environ["LINK_TO"]
         )
     ).read()
     try:
         total_time = int(re.findall("time (\\d+)ms", ping_output)[0])
         recvd = int(re.findall("([0-9]+) received", ping_output)[0])
-        requests.post(influxdb_url, influxdb_data.format(0.01 * recvd / (total_time / 1000)))
+        requests.post(influxdb_url, influxdb_data.format(0.0056 * recvd / (total_time / 1000)))
     except:
         assert False, sys.exc_info()
         requests.post(influxdb_url, influxdb_data.format(0))
